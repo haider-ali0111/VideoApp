0e6e49d9474c56d02b0c275284258d2171433095
haider-ali0111 <haideraliak91@gmail.com>
2025-05-09 00:52:22 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://videoappbackend-d4d9bhckaxg9eyhn.canadacentral-01.azurewebsites.net/api';
-
+const API_URL2 = 'https://videoappbackend-d4d9bhckaxg9eyhn.canadacentral-01.azurewebsites.net/api';
 export default {
     API_URL
 }; 
